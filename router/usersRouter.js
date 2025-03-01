60712e3dd579094223b967148033c9e5f73fa181
Likhon <likhon.929@gmail.com>
2025-03-01 23:41:30 +0600

    Add user creation route with validation checks

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,15 +1,26 @@
 // external imports
 const express = require('express');
+const { check } = require('express-validator');
 
 // internal imports
 const { getUsers } = require('../controller/userController');
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
+const avatarUpload = require('../middlewares/users/avatarUpload');
 
 // router
 const router = express.Router();
 
 // users page
 router.get('/', decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post("/", avatarUpload, [
+    check('name').not().isEmpty().withMessage('Name is required!'),
+    check('email').isEmail().withMessage('Invalid email address!'),
+    check('password').isLength({ min: 6 }).withMessage('Password must be 6 characters long!'),
+], (req, res) => {
+    res.send("Add user");
+});
 
 // export
 module.exports = router;
