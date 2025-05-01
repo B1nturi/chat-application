830d3307633adf3c2393393f6791518c66fe0c5f
Likhon <likhon.929@gmail.com>
2025-05-01 20:45:07 +0600

    feat: Enhance authentication middleware with role-based access control for user routes Completed!!!

--- middlewares/common/checkLogin.js
+++ middlewares/common/checkLogin.js
@@ -1,78 +1,81 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
 
+// auth guard to protect routes that need authentication
 const checkLogin = (req, res, next) => {
-    let cookie =
-        Object.keys(req.signedCookies).length > 0
-            ? req.signedCookies
-            : null;
-    if (cookie) {
-        try {
-            const token = cookie[process.env.COOKIE_NAME];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = decoded;
+  let cookies =
+    Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
 
-            // pass the user to the next middleware
-            if (res.locals.html) {
-                res.locals.loggedInUser = decoded;
-            }
-            next();
-        }
-        catch (err) {
-            if (res.locals.html) {
-                res.render('login', {
-                    errors: {
-                        common: {
-                            msg: err.message,
-                        },
-                    },
-                });
-            }
-            else {
-                res.status(500).json({
-                    errors: {
-                        common: {
-                            msg: "Authentication failed!",
-                        },
-                    },
-                });
-            }
-        }
-    } else {
-        if (res.locals.html) {
-            res.render('login', {
-                errors: {
-                    common: {
-                        msg: "Authentication failed!",
-                    },
-                },
-            });
-        }
-        else {
-            res.status(401).json({
-                errors: {
-                    common: {
-                        msg: "Authentication failed!",
-                    },
-                },
-            });
-        }
+  if (cookies) {
+    try {
+      token = cookies[process.env.COOKIE_NAME];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+
+      // pass user info to response locals
+      if (res.locals.html) {
+        res.locals.loggedInUser = decoded;
+      }
+      next();
+    } catch (err) {
+      if (res.locals.html) {
+        res.redirect("/");
+      } else {
+        res.status(500).json({
+          errors: {
+            common: {
+              msg: "Authentication failure!",
+            },
+          },
+        });
+      }
     }
+  } else {
+    if (res.locals.html) {
+      res.redirect("/");
+    } else {
+      res.status(401).json({
+        error: "Authetication failure!",
+      });
+    }
+  }
+};
+
+// redirect already logged in user to inbox pabe
+const redirectLoggedIn = function (req, res, next) {
+  let cookies =
+    Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
+
+  if (!cookies) {
+    next();
+  } else {
+    res.redirect("/inbox");
+  }
+};
+
+// guard to protect routes that need role based authorization
+function requireRole(role) {
+  return function (req, res, next) {
+    if (req.user.role && role.includes(req.user.role)) {
+      next();
+    } else {
+      if (res.locals.html) {
+        next(createError(401, "You are not authorized to access this page!"));
+      } else {
+        res.status(401).json({
+          errors: {
+            common: {
+              msg: "You are not authorized!",
+            },
+          },
+        });
+      }
+    }
+  };
 }
 
-const redirectLoggedIn = (req, res, next) => {
-    let cookies =
-        Object.keys(req.signedCookies).length > 0
-            ? req.signedCookies
-            : null;
-    if (!cookies) {
-        next();
-    } else {
-        res.redirect('/inbox');
-    }
-}
-
-// export
 module.exports = {
-    checkLogin, 
-    redirectLoggedIn
+  checkLogin,
+  redirectLoggedIn,
+  requireRole,
 };
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -7,19 +7,19 @@ const { getUsers, addUser, removeUser } = require('../controller/userController'
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
 const avatarUpload = require('../middlewares/users/avatarUpload');
 const { addUserValidators, addUserValidationHandler } = require('../middlewares/users/userValidators');
-const {checkLogin} = require('../middlewares/common/checkLogin');
+const {checkLogin, requireRole} = require('../middlewares/common/checkLogin');
 
 // router
 const router = express.Router();
 
 // users page
-router.get('/', checkLogin, decorateHtmlResponse("Users"), checkLogin, getUsers);
+router.get('/', checkLogin, decorateHtmlResponse("Users"), checkLogin, requireRole(["admin"]), getUsers);
 
 // add user
-router.post("/", avatarUpload, addUserValidators, addUserValidationHandler, addUser);
+router.post("/", checkLogin, requireRole(["admin"]), avatarUpload, addUserValidators, addUserValidationHandler, addUser);
 
 // remove user
-router.delete("/:id", removeUser);
+router.delete("/:id", checkLogin, requireRole(["admin"]), removeUser);
 
 // export
 module.exports = router;
